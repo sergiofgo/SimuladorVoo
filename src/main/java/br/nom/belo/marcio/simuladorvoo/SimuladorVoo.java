2612ac6392ee41cf11cd0ba4f67cdef55f4fe4b1
mbelo <mbelo.br@gmail.com>
2021-06-30 23:37:00 -0300

    Correcoes do lint de java

--- src/main/java/br/nom/belo/marcio/simuladorvoo/SimuladorVoo.java
+++ src/main/java/br/nom/belo/marcio/simuladorvoo/SimuladorVoo.java
@@ -1,10 +1,14 @@
 package br.nom.belo.marcio.simuladorvoo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class Aviao implements Runnable {
 
     private Aeroporto aeroporto;
     private String idAviao;
     private long tempoVoo=0;
+    private static final Logger LOG = LoggerFactory.getLogger( "Aviao");
 
     public Aviao(Aeroporto aeroporto, String idAviao,long tempoVoo) {
         this.aeroporto = aeroporto;
@@ -13,37 +17,44 @@ class Aviao implements Runnable {
     }
 
     public void run() {
-        try {
-            Thread.sleep(tempoVoo/2);
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
-        }
 
+        try {
+            
+            Thread.sleep( tempoVoo / 2);
+        } catch (InterruptedException ie) {
+            
+            LOG.info( AppMessages.MSG_THREAD_INTERROMPIDA);
+            Thread.currentThread().interrupt();
+        }
         decolar();
         voar();
         aterrisar();
-        
     }
 
     private void decolar() {
-        System.out.println(idAviao + ": esperando pista...");
-        String acao=idAviao + ": decolando...";
-        aeroporto.esperarPistaDisponivel(acao); // Espera uma pista livre
+        LOG.info( "{}: esperando pista...", idAviao);
+        String acao = idAviao + ": decolando...";
+        aeroporto.esperarPistaDisponivel( acao); // Espera uma pista livre
     }
 
     private void voar() {
-        System.out.println(idAviao + ": voando...");
+
+        LOG.info( "{}: voando...", idAviao);
         try {
-            Thread.sleep(tempoVoo);
+            
+            Thread.sleep( tempoVoo);
         } catch (InterruptedException e) {
-            System.err.println(e);
+            
+            LOG.error( AppMessages.MSG_THREAD_INTERROMPIDA);
+            Thread.currentThread().interrupt();
         }
     }
 
     private void aterrisar() {
-        System.out.println(idAviao + ": esperando pista...");
-        String acao=idAviao + ": aterissando...";
-        aeroporto.esperarPistaDisponivel(acao); // Espera uma pista livre
+
+        LOG.info( "{}: esperando pista...", idAviao);
+        String acao = idAviao + ": aterissando...";
+        aeroporto.esperarPistaDisponivel( acao); // Espera uma pista livre
     }
 }
 
@@ -51,68 +62,86 @@ class Aeroporto implements Runnable {
 
     private boolean temPistaDisponivel = true;
     private String nomeAeroporto;
+    private static final Logger LOG = LoggerFactory.getLogger( "Aeroporto");
 
     public Aeroporto(String nomeAeroporto) {
+        
         this.nomeAeroporto = nomeAeroporto;
     }
 
     public synchronized void esperarPistaDisponivel(String acao) {
-        System.out.println(acao);
+        
+        LOG.info( acao);
     }
 
     public synchronized void mudarEstadoPistaDisponivel() {
+        
         // Inverte o estado da pista.
         temPistaDisponivel = !temPistaDisponivel;
-        System.out.println(nomeAeroporto + " tem pista disponível: " + 
-                (temPistaDisponivel == true ? "Sim" : "Não"));
+
+        LOG.info( "{} tem pista disponível? {}", nomeAeroporto, (temPistaDisponivel ? "Sim" : "Não"));
+
         // Notifica a mudanca de estado para quem estiver esperando.
-        if(temPistaDisponivel) this.notify();
+        if( temPistaDisponivel) this.notify();
     }
 
     public void run() {
-        System.out.println("Rodando aeroporto " + nomeAeroporto);
+
+        LOG.info( "Rodando aeroporto {}", nomeAeroporto);
         while (true) {
             try {
                 mudarEstadoPistaDisponivel();
                 // Coloca a thread aeroporto dormindo por um tempo de 0 a 5s
                 Thread.sleep((int)(Math.random()*5000)); 
             } catch (InterruptedException e) {
-                System.err.println(e);
+
+                LOG.error( AppMessages.MSG_THREAD_INTERROMPIDA);
+                Thread.currentThread().interrupt();
             }
         }
     }
-
 }
 /*
  * Simulador de voo com threads
  */
 public final class SimuladorVoo {
+    
+    private static final Logger LOG = LoggerFactory.getLogger( "SimuladorVoo");
 
     public static void main(String[] args) {
 
-        System.out.println("Rodando simulador de voo.");
+        LOG.info( "Rodando simulador de voo.");
 
         // Constroi aeroporto e inicia sua execucao.
         // NÃO MEXER NESSE TRECHO
-        Aeroporto santosDumont = new Aeroporto("Santos Dumont");
-        Thread threadAeroporto = new Thread(santosDumont);
+        Aeroporto santosDumont = new Aeroporto( "Santos Dumont");
+        Thread threadAeroporto = new Thread( santosDumont);
 
         // Constrói aviao e inicia sua execucao.
         // NÃO MEXER NESSE TRECHO
-        Aviao aviao14bis = new Aviao(santosDumont, "Avião 14BIS",10000);
-        Thread thread14bis = new Thread(aviao14bis);
+        Aviao aviao14bis = new Aviao( santosDumont, "Avião 14BIS",10000);
+        Thread thread14bis = new Thread( aviao14bis);
 
         // Inicia as threads
         threadAeroporto.start();
         thread14bis.start();
 
         try {
+            
             // Junta-se ao término da execução da thread do aeroporto
             threadAeroporto.join();
         } catch (InterruptedException ex) {
-            ex.printStackTrace();
+            
+            LOG.error( AppMessages.MSG_THREAD_INTERROMPIDA);
+            Thread.currentThread().interrupt();
         }
-        System.out.println("Terminando thread principal.");
-
+        LOG.info( "Terminando thread principal.");
     }
+}
+
+class AppMessages {
+
+    static final String MSG_THREAD_INTERROMPIDA = "Thread interrompida";
+
+    private AppMessages() { }
 }
